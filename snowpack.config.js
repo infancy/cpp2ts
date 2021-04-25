f4a14c48d5cf98df11edc19e907eeb2d7a76975d
infancy <326948850@qq.com>
2021-04-26 00:05:47 +0800

    add snowpack.config.js

---
+++ snowpack.config.js
@@ -0,0 +1,33 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    /* ... */
+  },
+  plugins: [
+    '@snowpack/plugin-typescript',
+  ],
+  packageOptions: {
+    polyfillNode: true,
+    external: [
+      // ignore `import fs from 'fs'` etc
+      ...require('module').builtinModules,
+      // ignore things specified in pkg.dependencies, so that
+      // only pkg.devDependencies get handled by Snowpack
+      // (this can drastically reduce the size of a deployed
+      // app, since you can deploy an already-optimized
+      // web_modules and a tiny node_modules, instead of
+      // having web_modules duplicated in a huge node_modules)
+      
+      //...Object.keys(require('./package.json').dependencies)
+      ],
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+};
